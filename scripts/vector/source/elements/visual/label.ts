ce96ac7675c011373a912826de8c0ba27c6efe81
nicolai0001 <109861820+nicolai0001@users.noreply.github.com>
2022-07-23 10:16:24 -0500

    Delete scripts/vector directory

--- scripts/vector/source/elements/visual/label.ts
+++
@@ -1,3 +0,0 @@
-
-
-// TODO: a label is 
--- scripts/vector/source/util/svg.ts
+++
@@ -1,8 +0,0 @@
-/**
-* Parses and returns the SVG documented represented by the string argument.
-*/
-export function parseSVG(svg) : SVGElement {
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(svg, 'image/svg+xml');
-    return doc.documentElement as unknown as SVGElement;
-}
